991a9e43c902fcd1c40d8f52abec1a1ba632e779
monis <ahmadmonis371@gmail.com>
2023-02-25 10:35:02 +0530

    login done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::view('/','login');
+Route::post('/',[UserController::class,'login']);
+Route::get('/login',[ProductController::class,'index']);
